5ef0baf81941bb962789e80e939f4edf6d709118
Ali <alihussaini3612@gmail.com>
2024-10-29 12:56:49 +0330

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 'auto',
+  spaceBetween: 40,
+  centeredSlides : true,
+  grabCursor : true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
